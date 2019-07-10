6648506ca9ce1671bc2e5ed23e1244f64a6f403b
Ushieru Kokoran <32021976+USHIERU@users.noreply.github.com>
2019-07-10 12:31:12 -0500

    Update Haversine.dart

--- Haversine.dart
+++ Haversine.dart
@@ -1,21 +1,21 @@
 import 'dart:math';
 
 class Haversine {
-  static final R = 6372.8; //Earth's Radius
+  static final radius = 6372.8; //Earth's Radius
 
   static double distanceBetween(
       double startLatitude, startLongitude, endLatitude, endLongitude) {
-    return (R *
+    return (radius *
             (2 *
                 asin(sqrt(pow(
-                        sin(_mRadians(endLatitude - startLatitude) / 2), 2) +
-                    pow(sin(_mRadians(endLongitude - startLongitude) / 2), 2) *
-                        cos(_mRadians(startLatitude)) *
-                        cos(_mRadians(endLatitude)))))) *
+                        sin(_radians(endLatitude - startLatitude) / 2), 2) +
+                    pow(sin(_radians(endLongitude - startLongitude) / 2), 2) *
+                        cos(_radians(startLatitude)) *
+                        cos(_radians(endLatitude)))))) *
         1000;
   }
 
-  static double _mRadians(double degree) {
+  static double _radians(double degree) {
     return degree * pi / 180;
   }
 }
